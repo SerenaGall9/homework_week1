fe117ea8a14b124d48dc6bbc8e3fd63a7f2a2e4e
PinkPanda099 <anita.bolina@gmail.com>
2025-01-27 21:53:03 +0000

    Add files via upload

---
+++ Exercise8/operations.py
@@ -0,0 +1,10 @@
+var= input("Please enter a value: ")
+
+print(var.upper())
+# upper is the function making it upppercase. I had to put it as a dot after the variable to do the method (turning uppercase) to the variable and then printing it
+
+print(len(var))
+# len function counts the number of characters
+
+print(var.isdecimal())
+# prints true or false if the entire value or var is numerical or not
