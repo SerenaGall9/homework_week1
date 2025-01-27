d13112bbbe75e53960395a09b4de89ff1faa4091
Serena Gallinotti <serena.gallinotti@gmail.com>
2025-01-27 19:56:32 +0000

    ADD a new file

---
+++ Exercise8/say_my_name.py
@@ -0,0 +1,12 @@
+first_name= ('Serena')
+print(first_name)
+print(type(first_name))
+
+last_name=('Gallinotti')
+print(last_name)
+print(type(last_name))
+
+# concatenation
+full_name= (first_name + ' '+ last_name)
+print(full_name)
+print(type(full_name))
