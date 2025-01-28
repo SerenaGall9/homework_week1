b37385fa5aaae5647c47337fdab86429a62202ef
Serena Gallinotti <serena.gallinotti@gmail.com>
2025-01-28 07:40:25 +0000

    new files reinstated

---
+++ PythonProject/types_practice.py
@@ -0,0 +1,29 @@
+first_name= 'Serena'
+print(first_name)
+print(type(first_name))
+
+last_name= 'Gallinotti'
+print(last_name)
+print(type(last_name))
+
+full_name= (first_name + ' ' + last_name)
+print(full_name)
+print(type(full_name))
+
+# list
+first_name= 'Serena'
+last_name= 'Gallinotti'
+full_name= 'Serena Gallinotti'
+
+name_list= ['Serena', 'Gallinotti', 'Serena Gallinotti']
+print(type(name_list))
+print(name_list)
+
+names_dictionary= {
+    'first': 'Serena', 'last': 'gallinotti'
+                   }
+print(names_dictionary)
+print(type(names_dictionary))
+
+print(names_dictionary['first'])
+print(names_dictionary['last'])
