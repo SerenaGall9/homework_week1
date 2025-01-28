1cc021fe42c5e6665a645e33e5cf3eb3177f300a
Serena Gallinotti <serena.gallinotti@gmail.com>
2025-01-28 07:34:35 +0000

    UPDATED folder structure

--- PythonProject/operation.py
+++
@@ -1,9 +0,0 @@
-var= input('please enter a value: ')
-print(var.upper())
-# Makes the var in uppercase
-
-print(len(var))
-# counts the number of character in the word var
-
-print(var.isdecimal())
-#
