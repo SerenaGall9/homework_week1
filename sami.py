b5a1e5cb4d5c034867db7964dd010128130a8360
samiat kolawole <kolawolesammy@gmail.com>
2025-01-28 18:37:33 +0000

    EDIT: Added description to projectile variable, and added a new file for say_my_name

---
+++ sami.py
@@ -0,0 +1,15 @@
+# # Define variables
+first_name = "Sami"
+last_name = "Kolawole"
+
+print(first_name , last_name)
+
+list_name = [first_name, last_name]
+print(list_name)
+
+# # Create a dictionary with key-value pairs
+dict_name = {"firstname": first_name, "lastname": last_name}
+# Print the dictionary
+print(dict_name)
+# Print the first key value
+print(dict_name["firstname"])
